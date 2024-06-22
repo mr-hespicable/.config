381a5a78e0fb770d4ca8685a917624bffd0a0123
leon <leonhmcqueen@gmail.com>
2024-06-22 19:55:48 +0100

    updated i3status and nvim and i3

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -5,51 +5,37 @@ return {
         "nvimdev/dashboard-nvim",
         lazy = false, -- As https://github.com/nvimdev/dashboard-nvim/pull/450, dashboard-nvim shouldn't be lazy-loaded to properly handle stdin.
         opts = function()
-            local logo = [[
-             ██████╗ ██████╗ ██████╗ ██╗███╗   ██╗ ██████╗ ██╗         Z
-            ██╔════╝██╔═══██╗██╔══██╗██║████╗  ██║██╔════╝ ██║     Z    
-            ██║     ██║   ██║██║  ██║██║██╔██╗ ██║██║  ███╗██║  z       
-            ██║     ██║   ██║██║  ██║██║██║╚██╗██║██║   ██║╚═╝ z        
-            ╚██████╗╚██████╔╝██████╔╝██║██║ ╚████║╚██████╔╝██╗          
-             ╚═════╝ ╚═════╝ ╚═════╝ ╚═╝╚═╝  ╚═══╝ ╚═════╝ ╚═╝          
-        ]]
-
-            logo = string.rep("\n", 8) .. logo .. "\n\n"
-
             local opts = {
-                theme = "doom",
-                hide = {
-                    -- this is taken care of by lualine
-                    -- enabling this messes up the actual laststatus setting after loading a file
-                    statusline = false,
-                },
+                theme = "hyper",
                 config = {
-                    header = vim.split(logo, "\n"),
-
-                    -- stylua: ignore
-                    center = {
-                        { action = 'lua LazyVim.pick()()',                           desc = " Find File",       icon = " ", key = "f" },
-                        { action = "ene | startinsert",                              desc = " New File",        icon = " ", key = "n" },
-                        { action = 'lua LazyVim.pick("oldfiles")()',                 desc = " Recent Files",    icon = " ", key = "r" },
-                        { action = 'lua LazyVim.pick("live_grep")()',                desc = " Find Text",       icon = " ", key = "g" },
-                        { action = 'lua LazyVim.pick.config_files()()',              desc = " Config",          icon = " ", key = "c" },
-                        { action = 'lua require("persistence").load()',              desc = " Restore Session", icon = " ", key = "s" },
-                        { action = function() vim.api.nvim_input("<cmd>qa<cr>") end, desc = " Quit",            icon = " ", key = "q" },
+                    week_header = {
+                        enable = true,
                     },
-                    footer = function()
-                        local stats = require("lazy").stats()
-                        local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
-                        return {
-                            "⚡ Neovim loaded " .. stats.loaded .. "/" .. stats.count .. " plugins in " .. ms .. "ms",
-                        }
-                    end,
+                    shortcut = {
+                        { desc = "󰊳 Update", group = "@property", action = "Lazy update", key = "u" },
+                        {
+                            icon = " ",
+                            icon_hl = "@variable",
+                            desc = "Files",
+                            group = "Label",
+                            action = "Telescope find_files",
+                            key = "f",
+                        },
+                        {
+                            desc = " Apps",
+                            group = "DiagnosticHint",
+                            action = "Telescope app",
+                            key = "a",
+                        },
+                        {
+                            desc = " dotfiles",
+                            group = "Number",
+                            action = "Neotree action=focus source=filesystem position=float toggle=true dir=/home/leon/.config/ ",
+                            key = "d",
+                        },
+                    },
                 },
             }
-
-            for _, button in ipairs(opts.config.center) do
-                button.desc = button.desc .. string.rep(" ", 43 - #button.desc)
-                button.key_format = "  %s"
-            end
 
             -- close Lazy and re-open when the dashboard is ready
             if vim.o.filetype == "lazy" then
