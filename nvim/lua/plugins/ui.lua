d174c7106c2ef4272b093aef8c4dbe49d65bfde9
leon <leonhmcqueen@gmail.com>
2024-06-23 10:42:09 +0100

    more stuff

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -16,7 +16,7 @@ return {
                     shortcut = {
                         { desc = "󰊳 update", group = "@property", action = "Lazy update", key = "u" },
                         {
-                            icon = " ",
+                            icon = "",
                             icon_hl = "@variable",
                             desc = "files",
                             group = "Label",
@@ -27,7 +27,7 @@ return {
                             desc = " config",
                             group = "Number",
                             action = "Neotree action=focus source=filesystem position=float toggle=true dir=/home/leon/.config/nvim/",
-                            key = "d",
+                            key = "c",
                         },
                         {
                             desc = "❌ quit",
@@ -36,7 +36,11 @@ return {
                             key = "q",
                         },
                     },
-
+                    projects = {
+                        enable = true,
+                        limit = 8,
+                        action = "Telescope find_files cwd=",
+                    },
                     packages = { enable = true },
                 },
             }
@@ -54,5 +58,52 @@ return {
 
             return opts
         end,
+    },
+    {
+        "folke/noice.nvim",
+        event = "VeryLazy",
+        opts = {
+            lsp = {
+                override = {
+                    ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+                    ["vim.lsp.util.stylize_markdown"] = true,
+                    ["cmp.entry.get_documentation"] = true,
+                },
+            },
+            routes = {
+                {
+                    filter = {
+                        event = "msg_show",
+                        any = {
+                            { find = "%d+L, %d+B" },
+                            { find = "; after #%d+" },
+                            { find = "; before #%d+" },
+                        },
+                    },
+                    view = "mini",
+                },
+            },
+            presets = {
+                bottom_search = true,
+                command_palette = true,
+                long_message_to_split = true,
+            },
+            cmdline = {
+                enabled = true,
+                view = "cmdline",
+            },
+        },
+        -- stylua: ignore
+        keys = {
+            { "<leader>sn", "", desc = "+noice"},
+            { "<S-Enter>", function() require("noice").redirect(vim.fn.getcmdline()) end, mode = "c", desc = "Redirect Cmdline" },
+            { "<leader>snl", function() require("noice").cmd("last") end, desc = "Noice Last Message" },
+            { "<leader>snh", function() require("noice").cmd("history") end, desc = "Noice History" },
+            { "<leader>sna", function() require("noice").cmd("all") end, desc = "Noice All" },
+            { "<leader>snd", function() require("noice").cmd("dismiss") end, desc = "Dismiss All" },
+            { "<leader>snt", function() require("noice").cmd("pick") end, desc = "Noice Picker (Telescope/FzfLua)" },
+            { "<c-f>", function() if not require("noice.lsp").scroll(4) then return "<c-f>" end end, silent = true, expr = true, desc = "Scroll Forward", mode = {"i", "n", "s"} },
+            { "<c-b>", function() if not require("noice.lsp").scroll(-4) then return "<c-b>" end end, silent = true, expr = true, desc = "Scroll Backward", mode = {"i", "n", "s"}},
+        },
     },
 }
